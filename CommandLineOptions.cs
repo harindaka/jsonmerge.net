93ea4b9a97e3b2248f06e5f41263c268baaf5d53
Hemal Athulathmudali <hathulathmudali@virtusa.com>
2018-07-17 14:37:48 +0530

    added commandline parser and merge functionality

---
+++ CommandLineOptions.cs
@@ -0,0 +1,49 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+
+namespace jsonmerge.net
+{
+    public class CommandLineOptions
+    {
+        [Option('b', "base-json", Required = false, Default = "", HelpText = "(Mandatory) The base JSON to be overridden.")]
+        public string BaseJson { get; set; }
+
+        [Option("base-json-file", Required = false, Default = null, HelpText = "(Mandatory) Path to the file where the base JSON to be overridden is stored.")]
+        public string BaseJsonFile { get; set; }
+
+        [Option('r', "override-json", Required = false, Default = "", HelpText = "(Mandatory) The JSON to be used as the override.")]
+        public string OverrideJson { get; set; }
+
+        [Option("override-json-file", Required = false, Default = null, HelpText = "(Mandatory) Path to the file where the JSON to be used as the overridde is stored.")]
+        public string OverrideJsonFile { get; set; }
+
+        [Option("output-json-file", Required = false, Default = null, HelpText = "(Optional) Path to the output file. If file exists it will be overridden. If this option is not specified output will be written to standard output.")]
+        public string OutputJsonFile { get; set; }
+
+        [Option("comments-ignore", Required = false, Default = false, HelpText = "(Optional) Ignore comments in JSON. Default: false")]
+        public bool CommentsIgnore { get; set; }
+        
+        [Option("line-info-ignore", Required = false, Default = false, HelpText = "(Optional) Ignore line info in JSON. Default: false")]
+        public bool LineInfoIgnore { get; set; }
+        
+        [Option("array-concat", SetName = "array-handling", Required = false, Default = false, HelpText = "(Optional) Concats arrays when merging JSON. Default: false")]
+        public bool ArrayConcat { get; set; }
+
+        [Option("array-replace", SetName = "array-handling", Required = false, Default = true, HelpText = "(Optional) Replaces arrays when merging JSON. Default: true")]
+        public bool ArrayReplace { get; set; }
+        
+        [Option("array-union", SetName = "array-handling", Required = false, Default = false, HelpText = "(Optional) Unions arrays when merging JSON (remove duplicates). Default: false")]
+        public bool ArrayUnion { get; set; }
+        
+        [Option("array-merge", SetName = "array-handling", Required = false, Default = false, HelpText = "(Optional) Merge arrays when merging JSON. Default: false")]
+        public bool ArrayMerge { get; set; }
+        
+        [Option("null-ignore", Required = false, Default = false, HelpText = "(Optional) Ignore null fields when merging JSON. Default: false")]
+        public bool NullIgnore { get; set; }
+        
+        [Option("indent", Required = false, Default = false, HelpText = "(Optional) Format output as indented JSON. Default: false")]
+        public bool Indent { get; set; }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,155 @@
 ﻿using System;
-using JSON.Net;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CommandLine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace jsonmerge.net
 {
     class Program
     {
         static void Main(string[] args)
+        {                                   
+            var parser = new Parser(p =>
+            {
+                p.CaseSensitive = true;
+                p.EnableDashDash = true;
+                p.HelpWriter = Console.Out;
+                p.ParsingCulture = CultureInfo.InvariantCulture;
+            });
+
+            parser.ParseArguments<CommandLineOptions>(args)
+                .WithParsed<CommandLineOptions>(opts => ExecuteCommand(opts))
+                .WithNotParsed<CommandLineOptions>((errs) => HandleParseError(errs));
+                        
+        }
+
+        private static void ExecuteCommand(CommandLineOptions opts)
         {
-            
+            string baseJson = null;
+            string overrideJson = null;
+
+            if (String.IsNullOrWhiteSpace(opts.BaseJsonFile))
+            {
+                baseJson = opts.BaseJson;
+            }
+            else
+            {
+                baseJson = File.ReadAllText(opts.BaseJsonFile);
+            }
+
+            if (String.IsNullOrWhiteSpace(opts.OverrideJsonFile))
+            {
+                overrideJson = opts.OverrideJson;
+            }
+            else
+            {
+                overrideJson = File.ReadAllText(opts.OverrideJsonFile);
+            }
+                        
+            var loadSettings = new JsonLoadSettings();
+            if(opts.CommentsIgnore)
+            {
+                loadSettings.CommentHandling = CommentHandling.Ignore;
+            }
+            else
+            {
+                loadSettings.CommentHandling = CommentHandling.Load;
+            }
+
+            if (opts.LineInfoIgnore)
+            {
+                loadSettings.LineInfoHandling = LineInfoHandling.Ignore;
+            }
+            else
+            {
+                loadSettings.LineInfoHandling = LineInfoHandling.Load;
+            }
+
+            var mergeSettings = new JsonMergeSettings();
+            if (opts.ArrayConcat)
+            {
+                mergeSettings.MergeArrayHandling = MergeArrayHandling.Concat;
+            }
+            else if (opts.ArrayMerge)
+            {
+                mergeSettings.MergeArrayHandling = MergeArrayHandling.Merge;
+            }
+            else if (opts.ArrayReplace)
+            {
+                mergeSettings.MergeArrayHandling = MergeArrayHandling.Replace;
+            }
+            else if (opts.ArrayUnion)
+            {
+                mergeSettings.MergeArrayHandling = MergeArrayHandling.Union;
+            }
+
+            if (opts.NullIgnore)
+            {
+                mergeSettings.MergeNullValueHandling = MergeNullValueHandling.Ignore;
+            }
+            else
+            {
+                mergeSettings.MergeNullValueHandling = MergeNullValueHandling.Merge;
+            }
+
+            Formatting formatting = Formatting.None;
+            if(opts.Indent)
+            {
+                formatting = Formatting.Indented;
+            }
+
+            var mergedJson = MergeJson(baseJson, overrideJson, loadSettings, mergeSettings, formatting);
+
+            if(String.IsNullOrEmpty(opts.OutputJsonFile))
+            {
+                Console.Write(mergedJson);
+            }
+            else
+            {
+                File.WriteAllText(opts.OutputJsonFile, mergedJson);
+            }
+
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errors)
+        {
+            foreach(var error in errors)
+            {
+                Console.WriteLine(error.ToString());                
+            }
+        }
+
+        private static string MergeJson(
+            string baseJson, 
+            string overrideJson, 
+            JsonLoadSettings loadSettings,
+            JsonMergeSettings mergeSettings,
+            Formatting formatting
+            )
+        {
+            if (String.IsNullOrWhiteSpace(baseJson) && String.IsNullOrWhiteSpace(overrideJson))
+            {
+                return String.Empty;
+            }
+
+            if(String.IsNullOrWhiteSpace(baseJson))
+            {
+                baseJson = "{}";
+            }
+            else if (String.IsNullOrWhiteSpace(overrideJson))
+            {
+                overrideJson = "{}";
+            }
+
+            var baseObject = JObject.Parse(baseJson, loadSettings);
+            var overrideObject = JObject.Parse(overrideJson, loadSettings);
+
+            baseObject.Merge(overrideObject, mergeSettings);
+
+            return baseObject.ToString(formatting);
         }
     }
 }
